df48755d7088649bb8d399e8f02de49d22ce75f0
yanglvfeng <yanglvfeng@croot.com>
2021-05-21 15:30:17 +0800

    add 利用localStorage实现菜单记忆功能

--- src/Sider.tsx
+++ src/Sider.tsx
@@ -3,10 +3,15 @@ import {Link} from "react-router-dom";
 import {Menu} from "antd";
 import styles from  './Sider.module.scss';
 
+const saveKey:string = 'menu-default-select-key';
+const defaultSelectedKey = getDefaultSelectKey();
+
 export default function Sider(){
     return <Menu className={styles.sider}
-                 onClick={console.log}
-                 defaultSelectedKeys={['0']}
+                 onClick={({ item, key })=>{
+                     setDefaultSelectKey(key);
+                 }}
+                 defaultSelectedKeys={[defaultSelectedKey]}
                  style={{width:240}}>
         <Menu.Item key='0'>
             <Link to='/home'>首页</Link>
@@ -15,4 +20,15 @@ export default function Sider(){
             <Link to='/overtime-record'>加班记录</Link>
         </Menu.Item>
     </Menu>
+}
+
+function getDefaultSelectKey(){
+    const value = localStorage.getItem(saveKey);
+    if(value) return value;
+    localStorage.setItem(saveKey,'0');
+    return '0';
+}
+
+function setDefaultSelectKey(value: string | number){
+    localStorage.setItem(saveKey,value.toString())
 }
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Home(props) {
+export default function Home() {
     return (<div>
         这里是首页内容！
     </div>)
