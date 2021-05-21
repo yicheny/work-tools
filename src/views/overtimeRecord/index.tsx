4007be80397158a048205feb3793fe63828fc0de
yanglvfeng <yanglvfeng@croot.com>
2021-05-21 18:13:40 +0800

    update 加班记录 查询联调完成

--- src/views/overtimeRecord/index.tsx
+++ src/views/overtimeRecord/index.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import {Button} from 'antd';
 import {useOpenInfo} from "../../common/hooks";
 import AddModal from "./AddModal";
+import {Api} from "../../base";
+import {tryExecute} from "../../common/utils";
 
 export default function OvertimeRecord() {
    const {openInfo,setOpenInfo,close} = useOpenInfo();
+   const {data,query} = useTableData();
 
+    console.log('data',data);
     return <div>
-        <Button type='primary' onClick={()=>setOpenInfo({type:'add'})}>新增记录</Button>
+        <Button onClick={query}>查询</Button>
+        <Button type='primary' onClick={()=>setOpenInfo({type:'add'})}>新增</Button>
         <AddModal visible={openInfo.type==='add'} close={close}/>
     </div>
 };
+
+function useTableData(){
+    const [data,setData]  = useState([]);
+
+    const query = useCallback(()=>{
+        tryExecute(async ()=>{
+           const result:any = await Api.get(`/overtime-record/query`);
+           setData(result?.data);
+        })
+    },[])
+
+    return {data,query};
+}
